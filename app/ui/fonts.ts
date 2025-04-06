d9d8be11827e8b7778cc85979023431354f05842
Devonapta <dionisiushugosta87@gmail.com>
2025-04-06 22:44:04 +0700

    fonts.ts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,18 @@
-import { Inter, Lusitana } from 'next/font/google';
+  import { Inter, Lusitana, Stalinist_One, Rubik_Lines } from "next/font/google";
 
-export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lusitana({
-    weight: ['400', '700'],
-    subsets: ['latin']
-})
+  export const inter = Inter({ subsets: ["latin"] });
+
+  export const lusitana = Lusitana({
+    subsets: ["latin"],
+    weight: ["400", "700"], // Menggunakan array untuk konsistensi
+  });
+
+  export const stalinistOne = Stalinist_One({
+    subsets: ["latin"],
+    weight: ["400"], // Konsisten dengan array meskipun satu nilai
+  });
+  
+  export const rubikLines = Rubik_Lines({
+    subsets: ["latin"],
+    weight: ["400"], // Konsisten dengan array meskipun satu nilai
+  });
