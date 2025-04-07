cbba4025a59e50d3ad8e2db1d5224983d9a5e7d8
brumziee <bramantyof.w@gmail.com>
2025-04-07 21:57:21 +0700

    Update fonts.ts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
-  import { Inter, Lusitana, Stalinist_One, Rubik_Lines } from "next/font/google";
+  import { Inter, Lusitana, Stalinist_One, Rubik_Lines, Poppins } from "next/font/google";
 
   export const inter = Inter({ subsets: ["latin"] });
 
@@ -15,4 +15,9 @@
   export const rubikLines = Rubik_Lines({
     subsets: ["latin"],
     weight: ["400"], // Konsisten dengan array meskipun satu nilai
+  });
+
+  export const poppins = Poppins({
+    weight: ['400', '700'],
+    subsets: ['latin'],
   });
